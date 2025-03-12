c1b9c7d8de177f252023a10ac0813a83f87156c4
Cristian Goina <goinac@janelia.hhmi.org>
2025-03-12 18:35:37 -0400

    something that installs on windows

--- install-scripts/postinstall.bat
+++ install-scripts/postinstall.bat
@@ -1,3 +1,7 @@
-"%PREFIX%\python.exe" -m pip install git+https://github.com/funkelab/motile_tracker@installer
+set PATH=%PATH%;C:\ProgramData\scoop\shims
 
-echo Added by post-install script > "%PREFIX%\post_install_sentinel.txt"
+"%PREFIX%\Scripts\pip.exe" install "git+https://github.com/funkelab/motile_tracker@installer"
+
+echo complete post-install > "%PREFIX%\post_install.done"
+
+pause
