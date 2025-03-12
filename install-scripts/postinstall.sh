a1c827a789d108c073aa31a091742f1b410e338f
Cristian Goina <goinac@hhmi.org>
2025-03-11 21:51:29 -0400

    do not keep packages; activate env in post install script

--- install-scripts/postinstall.sh
+++ install-scripts/postinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-${PREFIX}/bin/pip install git+https://github.com/funkelab/motile_tracker@installer
+source "$PREFIX/etc/profile.d/conda.sh" && conda activate "$PREFIX"
+pip install git+https://github.com/funkelab/motile_tracker@installer
