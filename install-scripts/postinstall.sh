0cb5fda5e22ca8222610460be547bd48f6a5c80f
Cristian Goina <goinac@hhmi.org>
2025-03-13 16:55:25 -0400

    set startup script for osx and linux

--- install-scripts/postinstall.sh
+++ install-scripts/postinstall.sh
@@ -3,3 +3,7 @@
 source "$PREFIX/etc/profile.d/conda.sh" 
 conda activate "$PREFIX"
 "$PREFIX/bin/pip" install git+https://github.com/funkelab/motile_tracker@installer
+
+cat > "$PREFIX/motile-tracker.sh" <<EOF
+$PREFIX/bin/python -m motile_tracker.launcher
+EOF
