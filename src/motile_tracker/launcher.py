d6229bbb6f481a89ec5dc88075526ff8c69a506b
Cristian Goina <goinac@hhmi.org>
2025-03-21 17:25:32 -0400

    remove commented code

--- src/motile_tracker/launcher.py
+++ src/motile_tracker/launcher.py
@@ -1,8 +1,6 @@
 import multiprocessing
 import napari
 
-# from qtpy.QtWidgets import QApplication
-
 
 def launch_viewer():
     print('Open Napari Viewer with Motile Tracker plugin...')
@@ -14,7 +12,6 @@ def launch_viewer():
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     launch_viewer()
-    # app = QApplication.instance()
 
     # Start Napari event loop
     print('Start Napari event loop...')
