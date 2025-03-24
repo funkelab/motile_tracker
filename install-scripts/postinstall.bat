3535afdc00cbcf526b0ea5cc732cfc7a162fa2bb
Cristian Goina <goinac@hhmi.org>
2025-03-24 18:11:27 -0400

    removed constructor

--- install-scripts/postinstall.bat
+++
@@ -1,8 +0,0 @@
-"%PREFIX%\Scripts\pip.exe" install motile_tracker
-
-echo "Create start script"
-
-(
-    echo cd "%PREFIX%"
-    echo .\python -m motile_tracker.launcher
-)> "%PREFIX%\motile_tracker.bat"
--- install-scripts/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source "$PREFIX/etc/profile.d/conda.sh" 
-conda activate "$PREFIX"
-"$PREFIX/bin/pip" install motile_tracker
-
-cat > "$PREFIX/motile-tracker.sh" <<EOF
-$PREFIX/bin/python -m motile_tracker.launcher
-EOF
