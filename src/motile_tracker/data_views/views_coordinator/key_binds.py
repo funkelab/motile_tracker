4336bb5cb0e0399947db3f97a076eee6aaa94d9f
AnniekStok <anniek.stokkermans@gmail.com>
2025-10-10 16:27:06 +0200

    update docstrings and flip keymap

--- src/motile_tracker/data_views/views_coordinator/key_binds.py
+++ src/motile_tracker/data_views/views_coordinator/key_binds.py
@@ -1,17 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from napari.layers import Labels, Points
+
+    from motile_tracker.data_views.views.layers.track_labels import TrackLabels
+    from motile_tracker.data_views.views.layers.track_points import TrackPoints
+    from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
+
 KEYMAP = {
-    "q": "toggle_display_mode",
-    "a": "create_edge",
-    "d": "delete_node",
-    "Delete": "delete_node",
-    "b": "delete_edge",
-    "z": "undo",
-    "r": "redo",
+    "toggle_display_mode": ["q"],
+    "create_edge": ["a"],
+    "delete_node": ["d", "Delete"],
+    "delete_edge": ["b"],
+    "undo": ["z"],
+    "redo": ["r"],
 }
 
 
-def bind_keymap(target, keymap, tracks_viewer):
-    """Bind all keys in `keymap` to the corresponding methods on `tracks_viewer`."""
-    for key, method_name in keymap.items():
+def bind_keymap(
+    target: TrackPoints | Points | TrackLabels | Labels,
+    keymap: dict[str, str],
+    tracks_viewer: TracksViewer,
+):
+    """Bind all keys in `keymap` to the corresponding methods on `tracks_viewer` to the
+    target layer. This should be an instance of (Track)Labels or (Track)Points"""
+
+    for method_name, keys in keymap.items():
         handler = getattr(tracks_viewer, method_name, None)
         if handler is not None:
-            target.bind_key(key)(handler)
+            for key in keys:
+                target.bind_key(key)(handler)
