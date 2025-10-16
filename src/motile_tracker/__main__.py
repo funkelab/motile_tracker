3e9a50961d9150148cfb4d75dbdc4868e987d5f9
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-10-16 11:46:51 -0400

    fix: :bug: main script adds motile tracker tab twice

--- src/motile_tracker/__main__.py
+++ src/motile_tracker/__main__.py
@@ -3,15 +3,12 @@ import sys
 import napari
 
 from motile_tracker.application_menus.main_app import MainApp
-from motile_tracker.motile.menus.motile_widget import MotileWidget
 
 
-def main():
+def main() -> None:
     # Auto-load the motile tracker
     viewer = napari.Viewer()
     main_app = MainApp(viewer)
-    motile_widget = MotileWidget(viewer)
-    main_app.menu_widget.tabwidget.addTab(motile_widget, "Track with Motile")
     viewer.window.add_dock_widget(main_app)
 
     # Start napari event loop
