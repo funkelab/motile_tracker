6850c8cca83779910389df77da50c43222e41be5
AnniekStok <anniek.stokkermans@gmail.com>
2025-11-12 12:03:29 +0100

    remove filter_graph_with_ancestors function (now on funtracks)

--- src/motile_tracker/data_views/views_coordinator/groups.py
+++ src/motile_tracker/data_views/views_coordinator/groups.py
@@ -33,31 +33,6 @@ from motile_tracker.data_views.views.tree_view.tree_widget_utils import (
 if TYPE_CHECKING:
     from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
 
-import networkx as nx
-
-
-def filter_graph_with_ancestors(graph: nx.DiGraph, nodes_to_keep: set) -> list[int]:
-    """
-    Filter a graph to keep only the nodes in `nodes_to_keep` and their ancestors.
-
-    Args:
-        graph (nx.DiGraph): The original directed graph.
-        nodes_to_keep (set): The set of nodes to keep in the graph.
-
-    Returns:
-        list[int]: A subset of the original nodes in the graph containing only the nodes
-            in `nodes_to_keep` and their ancestors.
-    """
-    # Set to store all nodes to keep, including ancestors
-    all_nodes_to_keep = set(nodes_to_keep)
-
-    # Traverse ancestors for each node in nodes_to_keep
-    for node in nodes_to_keep:
-        ancestors = nx.ancestors(graph, node)  # Get all ancestors of the node
-        all_nodes_to_keep.update(ancestors)  # Add ancestors to the set
-
-    return list(all_nodes_to_keep)
-
 
 class CollectionButton(QWidget):
     """Widget holding a name and delete icon for listing in the QListWidget. Also contains
@@ -498,9 +473,6 @@ class CollectionWidget(QWidget):
 
         # Keep nodes that belong to the selected group
         nodes_to_keep = self.collection_list.itemWidget(item).collection
-        nodes_to_keep = filter_graph_with_ancestors(
-            self.tracks_viewer.tracks.graph, nodes_to_keep
-        )
 
         if export_type == "CSV":
             file_dialog = QFileDialog(self)
