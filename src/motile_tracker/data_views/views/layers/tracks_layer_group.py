65a41679f6943227219e2df316d16aa5d923b847
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2025-12-01 16:31:10 -0500

    Bug: pass visible tracks to tracks_layer update visible

--- src/motile_tracker/data_views/views/layers/tracks_layer_group.py
+++ src/motile_tracker/data_views/views/layers/tracks_layer_group.py
@@ -123,13 +123,20 @@ class TracksLayerGroup:
         if self.points_layer is not None:
             self.points_layer._refresh()
 
-    def update_visible(self, visible_nodes: list[int]):
+    def update_visible(self, visible_nodes: list[int] | str):
         if self.seg_layer is not None:
             self.seg_layer.update_label_colormap(visible_nodes)
         if self.points_layer is not None:
             self.points_layer.update_point_outline(visible_nodes)
         if self.tracks_layer is not None:
-            self.tracks_layer.update_track_visibility(visible_nodes)
+            # Convert node IDs to track IDs for the tracks layer
+            if isinstance(visible_nodes, str):
+                visible_tracks = visible_nodes  # "all"
+            else:
+                visible_tracks = list(
+                    {self.tracks.get_track_id(node) for node in visible_nodes}
+                )
+            self.tracks_layer.update_track_visibility(visible_tracks)
 
     def center_view(self, node):
         """Adjust the current_step and camera center of the viewer to jump to the node
