53445804416462bc47697a2d6dea3d2cc751b81a
AnniekStok <anniek.stokkermans@gmail.com>
2026-04-29 16:23:25 +0200

    update welcome widget

--- src/motile_tracker/application_menus/welcome_widget.py
+++ src/motile_tracker/application_menus/welcome_widget.py
@@ -13,6 +13,8 @@ TUTORIAL_URL = "https://github.com/funkelab/motile_tracker/blob/main/assets/moti
 
 
 class WelcomeWidget(QWidget):
+    """Getting started widget with links and basic information to get started with the tool."""
+
     def __init__(self, viewer: napari.Viewer):
         super().__init__()
 
@@ -60,16 +62,18 @@ class WelcomeWidget(QWidget):
 ### Quick Start
 
 1. **Load Data**: Drag and drop your label or points data in the napari viewer.
-2. **Configure Tracking**: Specify parameters in the Tracking panel and click 'Run Tracking'
-3. **Results**: Results are appended to the Tracks List. View and navigate a result in the Lineage View and Tracking segmentation and/or points layers
-4. **Edit Results**: Use the Editing & Selection panel to refine results
-5. **Visualize options**: Use the Visualization panel to adjust display and show orthogonal views.
+2. **Configure Tracking**: Specify parameters in the Tracking panel and click 'Run Tracking'.
+3. **Results**: Results are added to the Tracks List. View and navigate a tracking result in the napari layers and in the Lineage View.
+4. **Edit Results**: Use the Editing & Selection panel to refine results.
+5. **Visualization options**: Use the Visualization panel to adjust display mode and to show orthogonal views.
 6. **Save & Load**: Save or export results in the Tracks List. To pick up where you left off, load the project from Motile Run.
 
 ### Tips
 
-- Right-click on the 'eye' icon (middle) at the top of the docked widgets to configure which menus are visible.
-- Toggle panels with the `/` key to maximize viewing space
+- Right-click on the 'eye' icon (middle) at the top of the docked widgets to set menu visibility.
+- Toggle panels with the `/` key to maximize viewing space.
+- View individual lineages by changing the display mode in Visualization tab and in the Lineage View (press [Q])
+- If you have segmentation data, you can view additional features (e.g. area/volume) in the Lineage View (press [W])
 - Assign objects to custom groups to keep track of different cell populations or conditions ('Groups' menu).
 - Import data from external tracks from CSV or GEFF in the Tracks List menu.
         """)
