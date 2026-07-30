7bedf7c6a655260dae26be7f4192b2b9202f6c02
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2026-07-30 10:15:15 -0400

    Revert back the fixtures now that we are backward compat in funtracks

--- tests/conftest.py
+++ tests/conftest.py
@@ -205,7 +205,7 @@ def graph_3d() -> td.graph.GraphView:
 @pytest.fixture
 def graph_3d_without_segmentation(graph_3d: td.graph.GraphView) -> td.graph.GraphView:
     """Return a copy of graph_3d without segmentation-related node attributes."""
-    graph_without_seg = graph_3d.copy()
+    graph_without_seg = graph_3d.detach().filter().subgraph()
     graph_without_seg.remove_node_attr_key(td.DEFAULT_ATTR_KEYS.MASK)
     graph_without_seg.remove_node_attr_key(td.DEFAULT_ATTR_KEYS.BBOX)
     return graph_without_seg
@@ -214,7 +214,7 @@ def graph_3d_without_segmentation(graph_3d: td.graph.GraphView) -> td.graph.Grap
 @pytest.fixture
 def graph_2d_without_segmentation(graph_2d: td.graph.GraphView) -> td.graph.GraphView:
     """Return a copy of graph_2d without segmentation-related node attributes."""
-    graph_without_seg = graph_2d.copy()
+    graph_without_seg = graph_2d.detach().filter().subgraph()
     graph_without_seg.remove_node_attr_key(td.DEFAULT_ATTR_KEYS.MASK)
     graph_without_seg.remove_node_attr_key(td.DEFAULT_ATTR_KEYS.BBOX)
     graph_without_seg.remove_edge_attr_key("iou")
